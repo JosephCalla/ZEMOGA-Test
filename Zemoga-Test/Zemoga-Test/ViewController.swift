979fb54879bd6ac0823555c16312241e5646d82e
Joseph Calla <josephestanislao9@gmail.com>
2023-01-31 16:19:43 -0500

    Setup project

---
+++ Zemoga-Test/Zemoga-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zemoga-Test
+//
+//  Created by Joseph Estanislao Calla Moreyra on 31/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
